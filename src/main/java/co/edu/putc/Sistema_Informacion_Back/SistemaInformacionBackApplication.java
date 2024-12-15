4553e7caee2c622db66bb2a555f2d1e87b848018
alejandro202113019 <alejandro.mesa@uptc.edu.co>
2024-12-15 10:18:40 -0500

    Primer commit: subida inicial del proyecto

---
+++ src/main/java/co/edu/putc/Sistema_Informacion_Back/SistemaInformacionBackApplication.java
@@ -0,0 +1,13 @@
+package co.edu.putc.Sistema_Informacion_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInformacionBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInformacionBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/putc/Sistema_Informacion_Back/SistemaInformacionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.putc.Sistema_Informacion_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInformacionBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
